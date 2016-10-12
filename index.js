20915cf2f5edfdb6b8c8448c6c439ea064e3d017
Awpteamoose <awpteamoose@gmail.com>
2016-10-12 03:56:52 +0300

    it's easier to alias commands with this syntax, slight output tweak

--- index.js
+++ index.js
@@ -14,58 +14,56 @@ client.on("ready", () => {
 });
 
 var participants = [];
+var commands = {};
 
-var commands = {
-	"add": (msg) => {
-		if (msg.channel.name !== "lfg") return;
-		if (participants.some((p) => p.id === msg.author.id))
-			return msg.reply(`you're already added! ${participants.length}/12`);
+commands.add = (msg) => {
+	if (msg.channel.name !== "lfg") return;
+	if (participants.some((p) => p.id === msg.author.id))
+		return msg.reply(`you're already added! ${participants.length}/12`);
 
-		// unreachable in the current implementation
-		// if (participants.length >= 12)
-		//     return msg.reply(`sorry, mate, it's full! 12/12`);
+	// unreachable in the current implementation
+	// if (participants.length >= 12)
+	//     return msg.reply(`sorry, mate, it's full! 12/12`);
 
-		participants.push(msg.author);
-		msg.reply(`added! ${participants.length}/12`);
+	participants.push(msg.author);
+	msg.reply(`added! ${participants.length}/12`);
 
-		if (participants.length >= 12)
-		{
-			var mentions = "";
-			participants.forEach((p, i) => mentions += `${i+1}. <@${p.id}>\n`);
+	if (participants.length >= 12)
+	{
+		var mentions = "";
+		participants.forEach((p, i) => mentions += `${i+1}. <@${p.id}>\n`);
 
-			var cap1 = participants[randomInt(0, 11)];
-			participants = participants.filter((p) => p.id !== cap1.id);
-			var cap2 = participants[randomInt(0, 10)];
-			participants = participants.filter((p) => p.id !== cap2.id);
-			// participants now - players available for picking
+		var cap1 = participants[randomInt(0, 11)];
+		participants = participants.filter((p) => p.id !== cap1.id);
+		var cap2 = participants[randomInt(0, 10)];
+		participants = participants.filter((p) => p.id !== cap2.id);
+		// participants now - players available for picking
 
-			msg.channel.sendMessage("PUG is starting! Captains pick 1-2-2-2-2-1");
-			msg.channel.sendMessage(mentions);
-			msg.channel.sendMessage(`Your captains are <@${cap1.id}> (picks first, Team 1) and <@${cap2.id}>!`);
-			participants = [];
-		}
-	},
-	"remove": (msg) => {
-		if (msg.channel.name !== "lfg") return;
-		if (!participants.some((p) => p.id === msg.author.id))
-			return msg.reply(`you're not added! ${participants.length}/12`);
-
-		participants = participants.filter((p) => p.id !== msg.author.id);
-		msg.reply(`removed! ${participants.length}/12`);
-	},
-	"status": (msg) => {
-		if (participants.length == 0) {
-			return msg.reply("noone's signed up! 0/12");
-		}
-		var response = "participants are: ";
-		participants.forEach((p) => response += `${p.username}, `);
-		response = response.substring(0, response.length - 2);
-		msg.reply(`${response}. ${participants.length}/12`);
-	},
-	"help": (msg) => {
-		msg.reply(`available commands: !help, !status, !add (only #lfg), !remove (only #lfg). Once 12 players are added, the PUG will start and 2 random players will be chosen as captains. More advanced features coming soon.`);
+		msg.channel.sendMessage("PUG is starting! Captains pick 1-2-2-2-2-1\n" + mentions);
+		msg.channel.sendMessage(`Your captains are <@${cap1.id}> (picks first, Team 1) and <@${cap2.id}>!`);
+		participants = [];
 	}
-}
+};
+commands.remove = (msg) => {
+	if (msg.channel.name !== "lfg") return;
+	if (!participants.some((p) => p.id === msg.author.id))
+		return msg.reply(`you're not added! ${participants.length}/12`);
+
+	participants = participants.filter((p) => p.id !== msg.author.id);
+	msg.reply(`removed! ${participants.length}/12`);
+};
+commands.status = (msg) => {
+	if (participants.length == 0) {
+		return msg.reply("noone's signed up! 0/12");
+	}
+	var response = "participants are: ";
+	participants.forEach((p) => response += `${p.username}, `);
+	response = response.substring(0, response.length - 2);
+	msg.reply(`${response}. ${participants.length}/12`);
+};
+commands.help = (msg) => {
+	msg.reply(`available commands: !help, !status, !add (only #lfg), !remove (only #lfg). Once 12 players are added, the PUG will start and 2 random players will be chosen as captains. More advanced features coming soon.`);
+};
 
 
 client.on('message', msg => {
